b4c01fb6faa372830dc8fde8c4b578dfc395a044
Marina Haack <ninahaack@gmail.com>
2015-03-19 00:13:32 -0300

    testando

--- oi/src/com/example/oi/MainActivity.java
+++ oi/src/com/example/oi/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//oi gente
+		//oi gente -> Oi fofinha
 		setContentView(R.layout.activity_main);
 	}
 
